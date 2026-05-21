792108be4a13532adb5a20c29daeb5b3e6f6a10f
lopezsardip <lopezsardip@gmail.com>
2026-05-21 10:41:04 -0300

    Clase List terminada

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +10,30 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<Alumno> _alumno = new List<Alumno>();
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        _alumno.Add(alumno);
+    }
+
+    public List<Alumno> ObtenerLista()
+    {
+        return _alumno;
+    }
+
+    public Alumno? BuscarAlumno(string nombre)
+    {
+        return _alumno.Find(a => a.Nombre == nombre);
+    }
+
+    public void EliminarAlumno(Alumno alumno)
+    {
+        _alumno.Remove(alumno);
+    }
+
+    public void EliminarPosicion(int posicion)
+    {
+        _alumno.RemoveAt(posicion);
+    }
 }
